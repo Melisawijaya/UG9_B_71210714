68166b9311485fb2dfd0ba968384eee2060d6943
Melisawijaya <93273262+Melisawijaya@users.noreply.github.com>
2021-11-11 23:08:32 +0700

    71210714_Tambah Soal 3

---
+++ 3_B_71210714.py
@@ -0,0 +1,10 @@
+#input
+nilai1 = int(input("Nilai 1 : "))
+nilai2 = int(input("Nilai 2 : "))
+nilai3 = int(input("Nilai 3 : "))
+
+#process
+average = (nilai1 + nilai2 + nilai3)/3
+
+#output
+print("Rata-rata dari", nilai1, "+", nilai2, "+", nilai3, " adalah ", average)
