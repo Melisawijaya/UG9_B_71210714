f6654b8d6c87d09a92457464b5f90a8734c1a985
Melisawijaya <93273262+Melisawijaya@users.noreply.github.com>
2021-11-11 23:08:04 +0700

    71210714_Tambah Soal 2

---
+++ 2_B_71210714.py
@@ -0,0 +1,10 @@
+#input
+r = int(input("Masukkan jari-jari alas tabung: "))
+t = int(input("Masukkan tinggi tabung: "))
+phi = 3.14
+
+#process
+v = phi * r**2 * t
+
+#output
+print("Volume tabung adalah ", v)
